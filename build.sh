60a640ef3436e3501ce387f4b39e0b466d29eb0b
presly <presly>
2017-12-12 16:35:25 +0530

    build

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
+git stash -u && git checkout master
 git pull origin master
+git submodule update
 git submodule foreach git checkout master &&  git pull origin master
 mvn clean install
 docker build --force-rm -t node1:5000/ebuy-cart-service .
